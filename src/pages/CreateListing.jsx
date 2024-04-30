d9203e160696f596f30701f015c0a2d38922a439
leem-dev <alabitseleem@gmail.com>
2024-04-30 09:30:26 +0100

    completion of the UI for createListing

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -13,6 +13,7 @@ export default function CreateListing() {
     description: "",
     offer: false,
     regularPrice: 0,
+    discountedPrice: 0,
   });
 
   const {
@@ -26,6 +27,7 @@ export default function CreateListing() {
     description,
     offer,
     regularPrice,
+    discountedPrice,
   } = formData;
   function onChangeHandler() {}
 
@@ -219,6 +221,53 @@ export default function CreateListing() {
             </div>
           </div>
         </div>
+        {offer && (
+          <div className="flex items-center mb-6">
+            <div>
+              <p className="text-lg font-semibold">Discounted Price</p>
+              <div className="flex w-full justify-center items-center space-x-6">
+                <input
+                  type="number"
+                  id="discountedPrice"
+                  value={discountedPrice}
+                  onChange={onChangeHandler}
+                  min="50"
+                  max="400000000"
+                  required={true}
+                  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-250 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"
+                />
+                {type === "sale" && (
+                  <div className="">
+                    <p className="text-md w-full whitespace-nowrap">
+                      $ / Month
+                    </p>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="mb-6 ">
+          <p className="text-lg font-semibold ">Images</p>
+          <p className="text-gray-600 ">
+            The first image will be the cover (max 6)
+          </p>
+          <input
+            type="file"
+            id="images"
+            onChange={onChangeHandler}
+            accept=".jpg,.png,.jpeg"
+            multiple
+            required
+            className="w-full px-3 py-1.5 text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:border-slate-600 "
+          />
+        </div>
+        <button
+          type="submit"
+          className="mb-6 w-full px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+          create listing
+        </button>
       </form>
     </main>
   );
