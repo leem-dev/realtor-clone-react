8cf62b5b991018b17c8700d23d4b6091475d556a
leem-dev <alabitseleem@gmail.com>
2024-05-05 22:28:21 +0100

    postman verification

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import Spinner from "../component/Spinner";
 import { toast } from "react-toastify";
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 import { getAuth } from "firebase/auth";
-import {v4 as uuidv4} from "uuid"
+import { v4 as uuidv4 } from "uuid";
 
 export default function CreateListing() {
-  const auth = getAuth()
+  const auth = getAuth();
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
@@ -70,7 +75,7 @@ export default function CreateListing() {
     }
   }
 
-  function onSubmitForm(e) {
+  async function onSubmitForm(e) {
     e.preventDefault();
     setLoading(true);
     if (discountedPrice >= regularPrice) {
@@ -92,48 +97,55 @@ export default function CreateListing() {
 
     async function storeImage(image) {
       return new Promise((resolve, reject) => {
-        const storage = getStorage()
-        const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+        const storage = getStorage();
+        const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
         const storageRef = ref(storage, filename);
         const uploadTask = uploadBytesResumable(storageRef, image);
 
-        uploadTask.on('state_changed',
+        uploadTask.on(
+          "state_changed",
           (snapshot) => {
             // Observe state change events such as progress, pause, and resume
             // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
             switch (snapshot.state) {
-              case 'paused':
-                console.log('Upload is paused');
+              case "paused":
+                console.log("Upload is paused");
                 break;
-              case 'running':
-                console.log('Upload is running');
+              case "running":
+                console.log("Upload is running");
+                break;
+              default:
+                console.log("Upload not done");
                 break;
             }
-            }, 
-            (error) => {
-              // Handle unsuccessful uploads
-              reject(error)
-            }, 
-            () => {
-              // Handle successful uploads on complete
-              // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                resolve(downloadURL);
-              });
-            }
+          },
+          (error) => {
+            // Handle unsuccessful uploads
+            reject(error);
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
         );
-      })
+      });
     }
 
     const imgUrls = await Promise.all(
-      [...images].map((image) => storeImage(image)).catch((error) => {
-        setLoading(false)
-        toast.error("Images not uploaded")
-        return
-      })
-    )
+      [...images].map((image) => storeImage(image))
+    ).catch((error) => {
+      setLoading(false);
+      toast.error("Images not uploaded");
+      return;
+    });
+
+    console.log(imgUrls);
   }
 
   if (loading) {
