bae55664dca824277ae9ec4b8711c44cb8ff8345
leem-dev <alabitseleem@gmail.com>
2024-05-07 11:14:40 +0100

    Install react moment to keep track of time

--- src/component/ListingItem.jsx
+++ src/component/ListingItem.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import Moment from "react-moment";
+import { Link } from "react-router-dom";
+import { MdLocationOn } from "react-icons/md";
 
 export default function ListingItem({ listing, id }) {
-  return <div>{listing.name}</div>;
+  return (
+    <li>
+      <Link to={`/category/${listing.type}/${id}`}>
+        <img src={listing.imgUrls[0]} alt="" />
+        <Moment fromNow>{listing.timestamp?.toDate()}</Moment>
+        <div>
+          <div>
+            <MdLocationOn />
+            <p>{listing.address}</p>
+          </div>
+          <p>{listing.name}</p>
+          <p>
+            $
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" && " / month"}
+          </p>
+        </div>
+      </Link>
+    </li>
+  );
 }
