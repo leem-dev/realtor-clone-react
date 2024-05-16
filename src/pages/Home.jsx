9c0e651729cb9164368e53ec6588d1750f602f68
leem-dev <alabitseleem@gmail.com>
2024-05-16 22:33:50 +0100

    create the offer section for the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "../component/Slider";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
 
 export default function Home() {
+  // Offers
+  const [offerListings, setOfferListings] = useState(null);
+
+  useEffect(() => {
+    async function fetchOfferListings() {
+      try {
+        // get reference
+        const listingsRef = collection(db, "listings");
+        // create the query
+        const q = query(
+          listingsRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(4)
+        );
+        // execute the query
+        const querySnap = await getDocs(q);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setOfferListings(listings);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchOfferListings();
+  }, []);
+
   return (
     <div>
       <Slider />
+      <div className="max-w-6xl mx-auto pt-4 space-y-6">
+        {offerListings && offerListings.length > 0 && (
+          <div>
+            <h2>Recent Offers</h2>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
